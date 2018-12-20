6f241161f15df0307cc4b7daa6c77668299656fb
lemon <925349824@qq.com>
2018-12-20 10:45:37 +0800

    add react context demo

--- src/App.js
+++ src/App.js
@@ -5,17 +5,50 @@ import LikesButton from './component/LikesButton'
 import DigitalClock from './component/DigitalClock'
 import ComponentBox from './component/ComponentBoxControled'
 import GuestBook from './component/GuestBook'
+import ThemeContext from './theme-context'
+import ThemeBar from './component/ThemeBar'
+
+const themes={
+    light:{
+        className:'btn btn-primary',
+        bgColor:'#eeeeee',
+        color:'#000'
+    },
+    dark:{
+        className:'btn btn-light',
+        bgColor:'#222222',
+        color:'#fff'
+    }
+}
 
  const tags=['天才少女','躺鸡专业户'];
 class App extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            theme:'light'
+        }
+        this.changeTheme=this.changeTheme.bind(this);
+    }
+    changeTheme(theme){
+        this.setState({
+            theme
+        });
+    }
     render(){
-        return (<div className="App">
+        return (
+            <ThemeContext.Provider value={themes[this.state.theme]}>
+        <div className="App">
             <NameCard name="viking" number={18284548644} isHuman={true} tags={tags}/>
             <LikesButton/>
             <DigitalClock/>
             <ComponentBox/>
             <GuestBook/>
-        </div>);
+            <a href="#theme-switcher" className="btn btn-light" onClick={()=>{this.changeTheme('light')}}>浅色主题</a>
+            <a href="#theme-switcher" className="btn btn-secondary" onClick={()=>{this.changeTheme('dark')}}>深色主题</a>
+            <ThemeBar/>
+        </div>
+        </ThemeContext.Provider>);
     }
 }
 export default App;
